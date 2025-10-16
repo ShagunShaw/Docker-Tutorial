bc260530babb2c37df5bc65476c3b8ea7314748a
Shagun Shaw <shagunshaw2006@gmail.com>
2025-10-17 01:26:30 +0530

    Initial Commit 1

---
+++ Docker Tutorial/main.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => res.json({ message: 'Hey I am nodejs in container' }));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ Dockerfile Tutorial 2/hello.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello, World!"
+
+# Logic for factorial of a number
+echo "Enter a number to calculate its factorial:"
+read num
+fact=1
+
+for ((i=1; i<=num; i++)); do
+    fact=$((fact * i))
+done
+
+echo "Factorial of $num is $fact"
